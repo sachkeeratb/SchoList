929dfa52232548cc051da4b9a0f702eb3c22e2c2
Sachkeerat <61165141+sachkeeratb@users.noreply.github.com>
2024-02-20 12:03:06 -0500

    Delete backend directory

--- backend/src/constants.ts
+++
@@ -1,3 +0,0 @@
-// This file is to store certain constants as simple keywords
-
-export const __prod__ = process.env.NODE_ENV === 'production' 
--- backend/src/types.ts
+++
@@ -1,9 +0,0 @@
-import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core"; // ORM imports
-import { Request, Response } from "express"; // Express imports for requests and responses
-import { Session } from "express-session"; // Import the session
-
-export type MyContext = {
-  em: EntityManager<IDatabaseDriver<Connection>>;
-  req: Request & { session?: Session };
-  res: Response;
-};
