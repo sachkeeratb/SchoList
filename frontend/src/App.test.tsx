dc8035b838dfb955ef908dea775d4c381ae6f2d0
Sachkeerat <61165141+sachkeeratb@users.noreply.github.com>
2024-02-20 12:03:16 -0500

    Delete frontend directory

--- frontend/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-// Import react, the app, and the rendering
-import React from "react"
-import { App } from "./App"
-import { render } from "./test-utils"
-
-test("renders", () => {
-  render(<App />)
-})
